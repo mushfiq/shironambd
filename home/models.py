96fcfb1c1a02bbc66d2c6406ea3fe56ca7c860b0
Mushfiq <mushfiq@newscred.com>
2012-11-05 01:18:41 +0600

    added basic news model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+	title = models.CharField(max_length = 255)
+	pub_date = models.DateTimeField('date published')
+	create_date = models.DateTimeField('date of creation')
+	link = models.URLField(max_length = 255)
+	total_comments = models.IntegerField()
+	# source_id = models.ForeignKey(Source, to_field='id')
+	# user_id = models.ForeignKey(User, to_field='id')
+	
+	def __unicode__(self):
+		return self.title
+		
+	class Meta:
+		db_table = 'news'
+		
+class Source(models.Model):
+	name = models.CharField(max_length = 50)
+	website = models.CharField(max_length = 50)
+	
+	class Meta:
+		db_table = 'source'
