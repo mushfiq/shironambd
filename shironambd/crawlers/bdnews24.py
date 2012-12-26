d93cc3417a041ed87ed632bebaf1e0a6aebdb8ca
mushfiq <mushfiq.moon@gmail.com>
2012-12-27 01:59:50 +0600

    getting box newses from bdnews24 and added sample mongo insert script named mongo.py

--- shironambd/crawlers/bdnews24.py
+++ shironambd/crawlers/bdnews24.py
@@ -21,15 +21,34 @@ class BdNews24(BaseCrawler):
 		
 	def get_latest_news(self):
 		soup = self.get_soup()
-		latest_newses = soup.findAll('li',{'class':'latestnews'})
+		main_box = soup.find('div',{'id':'thdivbox'})
+		latest_newses = main_box.findAll('li',{'class':'latestnews'})
 		latest_news = latest_newses[13:]
-		print latest_newses
-
+		for news in latest_newses:
+			print news.text
+		# print latest_news[1].text
+		# for news in latest_news:
+		# 		yield news
+		
+	def get_box_news(self):
+		soup = self.get_soup()
+		box_soup_left = soup.findAll('div',{'id':'hmdivbox1'})
+		box_soup_right = soup.findAll('div',{'id':'hmdivbox2'})
+		box_left = []
+		box_right = []
+		for box in box_soup_left:
+			box_left.append(box.find('span').text)
+		
+		for box in box_soup_right:
+			box_right.append(box.find('span').text)
+		
+		return box_left+box_right
+			
 
 if __name__ == '__main__':
-	bUrl = 'http://bdnews24.com/'
+	bUrl = 'http://scrape.local/'
 	bdN = BdNews24(bUrl)
-	bdN.get_lead_news()
+	bdN.get_box_news()
 	
 	
 
