5e90c14fe861f2d41371329707a2518b7469fd10
Mushfiq <mushfiq@newscred.com>
2012-12-27 01:59:50 +0600

    getting box newses from bdnews24 and added sample mongo insert script named mongo.py

--- shironambd/crawlers/mongo.py
+++ shironambd/crawlers/mongo.py
@@ -1,4 +1,19 @@
 import setup_django
-from shironambd.home.models import Source
-s = Source()
-print "asda"
+from bdnews24 import *
+from shironambd.home.models import News
+
+
+
+def run():
+	bUrl = 'http://scrape.local/'
+	bdN = BdNews24(bUrl)	
+	for ne in bdN.get_box_news():
+		n = News()
+		n.title = ne
+		n.link = 'http://dummy.com'
+		n.save()
+		print n.title+"saved"
+		
+	
+if __name__=='__main__':
+	run()
