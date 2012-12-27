b8cc39fc867cc2727549e4ac2265c4b76d6109d0
Mushfiq <mushfiq@newscred.com>
2012-12-27 21:17:26 +0600

    added bdnews24.com english news crawler

---
+++ shironambd/crawlers/bdnews24bangla.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+bdnews24bangla.py
+
+Created by Caveman on 2012-12-27.
+Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
