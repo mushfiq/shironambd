f2e543c8268db97a706d337a0e36e6f9ad903d68
mushfiq <mushfiq.moon@gmail.com>
2013-01-18 11:21:30 +0600

    added intial version of banglanews crawler

---
+++ shironambd/crawlers/banglanews24.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+banglanews24.py
+
+Created by Caveman on 2013-01-01.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	table = soup.find('table', {'width':'984'})s
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
