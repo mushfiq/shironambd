8422e2e1d1dc5ace8873f311f8ecc7a5b9cc8611
Mushfiq <mushfiq@newscred.com>
2013-02-01 12:27:24 +0600

    removed unwanted file

---
+++ shironambd/crawlers/datahub.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+mongohub.py
+
+Created by Caveman on 2013-01-31.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+from bdnews24 import BdNews24
+
+
+def main():
+	bUrl = 'http://bangla.bdnews24.com/'
+	bdnews = BdNews24(bUrl)
+	
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
