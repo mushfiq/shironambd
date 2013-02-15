9c767c490df1a94dc8e06b9dd1f1178b852e1fc3
Mushfiq <mushfiq@newscred.com>
2013-02-15 19:31:08 +0600

    updated data crawling for banglanews24 crawler and fixed error,added template checking

--- shironambd/crawlers/banglanews24.py
+++ shironambd/crawlers/banglanews24.py
@@ -14,7 +14,7 @@ import os
 from base import BaseCrawler
 from datetime import datetime
 # from shironambd.home.models import News
-from utils import build_news_object
+from utils import build_news_object,is_valid
 
 BASE_URL = 'http://banglanews24.com/'
 TOTAL_MOST_READ = 7
@@ -34,36 +34,40 @@ def clean_text(text):
 
 class BanglaNews24(BaseCrawler):
 	
+	def get_tabs_news(self):
+		soup = self.get_soup()
+		return soup.find('div', {'class':'D01Tabs'})
+	
+	def process_and_save(self, news):
+		news_object = build_news_object(news)
+		if is_valid(news_object):
+			news_object.link = BASE_URL+str(news_object.link)
+			# print news_object
+			self.do_save(news_object)
+		
+	
 	def get_lead_news(self):
 		soup = self.get_soup()
 		raw_lead = soup.find('div',{'id':'eMythSlide'})
 		lead_soup = raw_lead.findAll('li')
 		for news in lead_soup:
-			try:
-				n = build_news_object(news)
-				n.title = clean_text(n.title)
-				print n
-			except Exception,e:
-				print "Error Occured! %s" %e
-				pass
-				
-	def get_tabs_news(self):
-		soup = self.get_soup()
-		return soup.find('div', {'class':'D01Tabs'})
+			self.process_and_save(news)
+			
 	
 	def get_latest_news(self):
 		tabs = self.get_tabs_news()
 		latest_soup = tabs.find('div',{'id':'tab1'})
 		latest_news = latest_soup.findAll('li')
 		for news in latest_news:
-			return build_news_object(news)
+			self.process_and_save(news)
 			
 	def get_best24(self):
 		tabs = self.get_tabs_news()
+		
 		best_24_soup = tabs.find('div', {'id':'tab2'})
 		best_24_news = best_24_soup.findAll('li')
 		for news in best_24_news:
-			return build_news_object(news) 
+			self.process_and_save(news)
 			
 	def get_most_read(self):
 		tabs = self.get_tabs_news()
@@ -71,7 +75,8 @@ class BanglaNews24(BaseCrawler):
 		most_read_news = most_read_soup.findAll('li')
 		if most_read_news:
 			for news in most_read_news:
-				print build_news_object(news)
+				# print news
+				self.process_and_save(news)
 		else:
 			return None
 		
@@ -83,10 +88,9 @@ class BanglaNews24(BaseCrawler):
 			categorized_news = soup.findAll('li', {'class':'liHead'})
 			print "Geting %s News" % news_category
 			for news in categorized_news:
-				print build_news_object(news)
-
+				self.process_and_save(news)
 
 if __name__ == '__main__':
 	b_url = 'http://banglanews24.com/'
 	BN24 = BanglaNews24(b_url)
-	BN24.get_categorized_news()
+	BN24.get_best24()
--- shironambd/crawlers/datahub.py
+++ shironambd/crawlers/datahub.py
@@ -11,6 +11,7 @@ import sys
 import os
 
 from bdnews24 import BdNews24
+from banglanews24 import BanglaNews24
 
 
 def crawl_bdnews24():
@@ -21,9 +22,20 @@ def crawl_bdnews24():
 	bdN.get_most_read()
 	bdN.get_recent_stories()
 	bdN.get_categorized_news()
+
+def crawl_banglanews24():
+	bUrl = 'http://banglanews24.com/'
+	bn24 = BanglaNews24(bUrl)
+	# bn24.get_lead_news()
+	# bn24.get_latest_news()
+	# bn24.get_categorized_news()
+	bn24.get_best24()
+	bn24.get_most_read()
 	
+		
 
 
 if __name__ == '__main__':
-	crawl_bdnews24()
+	# crawl_bdnews24()
+	crawl_banglanews24()
 
