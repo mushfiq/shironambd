71052fffa78a73391324ce50bdc4db624995ecd0
Mushfiq <mushfiq@newscred.com>
2013-02-22 01:54:07 +0600

    added celery specfic scripts

---
+++ shironambd/crawlers/tasks.py
@@ -0,0 +1,11 @@
+import requests
+from celery.decorators import periodic_task
+from datetime import timedelta
+from datahub import crawl_bdnews_latest_news
+
+@periodic_task(run_every=timedelta(seconds=5))
+def check_status():
+	print "Staring operation......."
+	crawl_bdnews_latest_news()
+	# res = requests.get(url)
+	# print res.status_code
