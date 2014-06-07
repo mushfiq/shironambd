c21cc342d7d58b22de26eec2cb2a4c4bd3bb4fb2
Mushfiq <mushfiq.moon@gmail.com>
2014-06-08 00:24:27 +0200

    updated source model

--- shironambd/home/models.py
+++ shironambd/home/models.py
@@ -3,37 +3,39 @@ import mongoengine as mongo
 
 
 class Source(mongo.Document):
-	name = mongo.StringField(max_length=50, required=True)
-	website = mongo.StringField(max_length=50)
-	logo = mongo.ImageField()
+    name = mongo.StringField(max_length=50, required=True)
+    website = mongo.StringField(max_length=50)
+    logo = mongo.ImageField()
+    parser_name = mongo.StringField(default=None, required=True)
+    urls = mongo.ListField(mongo.StringField(max_length=500))
+    def __unicode__(Source):
+    	return Source.name
 	
-	def __unicode__(Source):
-		return Source.name
-		
-	meta = {
-		'collection': 'sources'
-	}
+    meta = {
+    	'collection': 'sources'
+    }
 	
 	
 class News(mongo.Document):
-	title = mongo.StringField(required=True)
-	source = mongo.ReferenceField(Source)
-	published_at = mongo.DateTimeField(default=datetime.now())
-	created_at = mongo.DateTimeField(default=datetime.now())
-	link = mongo.URLField(required=True)
-	total_comments = mongo.IntField(default=0)
-	tags = mongo.ListField(mongo.StringField(max_length=50))
-	category = mongo.StringField(max_length=20)
-	is_featured = mongo.BooleanField(default=False)
+    title = mongo.StringField(required=True)
+    source = mongo.ReferenceField(Source)
+    author = mongo.StringField(default=False)
+    published_at = mongo.DateTimeField(default=datetime.now())
+    created_at = mongo.DateTimeField(default=datetime.now())
+    link = mongo.URLField(required=True)
+    total_comments = mongo.IntField(default=0)
+    tags = mongo.ListField(mongo.StringField(max_length=50))
+    category = mongo.StringField(max_length=20)
+    is_featured = mongo.BooleanField(default=False)
 
-	def __unicode__(News):
-		return News.title
-	
-	meta = {
-		'collection' : 'news',
-		'indexes' : ['title'],
-		'ordering' : ['-published_at']
-	}
+    def __unicode__(News):
+    	return News.title
+
+    meta = {
+    	'collection' : 'news',
+    	'indexes' : ['title'],
+    	'ordering' : ['-published_at']
+    }
 		
-	class Meta:
-		get_latest_by = 'created_at'
+    class Meta:
+    	get_latest_by = 'created_at'
