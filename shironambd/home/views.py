7af8bbe71ee9f4a5c59e3125da9dcf0152f39cf7
Mushfiq <mushfiq.moon@gmail.com>
2014-06-09 02:36:13 +0200

    added logos and misc changes for new style

--- shironambd/home/views.py
+++ shironambd/home/views.py
@@ -19,7 +19,7 @@ def index(request):
     all_news = []
     #TODO:optmiize this code 
     for source in all_sources:
-        news = News.objects.filter(source=source)[:10]
+        news = News.objects.filter(source=source)[:20]
         if len(news):
             for n in news:
                 all_news.append(n)
